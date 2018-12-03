d7fcdba8ae2443469ce81cb95aa642bffad4e2a4
Kristiyan Pelteshki <kpelt@softserveinc.com>
2018-12-03 18:46:58 +0200

    Revert "Merge pull request #4 from chavdarovgospodin/small-changes"
    This reverts commit 0c7d9b3f2e9bb6a40e0113f679c5473d83ad28f7, reversing
    changes made to a8c61502262560db31a0b46b33183c5c8745f7ac.

--- studioactions/common/AS_FlexContainer_f1ad9ecd7db1470aaecff1939dc175f2.js
+++
@@ -1,4 +0,0 @@
-function AS_FlexContainer_f1ad9ecd7db1470aaecff1939dc175f2(eventobject) {
-    var self = this;
-    return self.navToProfile.call(this);
-}
--- studioactions/common/AS_FlexContainer_ic20e6278944450e925a03c251c922b1.js
+++
@@ -1,4 +0,0 @@
-function AS_FlexContainer_ic20e6278944450e925a03c251c922b1(eventobject) {
-    var self = this;
-    return self.navToLogin.call(this);
-}
