8f87d913ec5ee9fecc883a957670eb5a5f940089
Kristiyan Pelteshki <kpelt@softserveinc.com>
2018-12-03 18:44:14 +0200

    header navigation
    made the sign up, backButton and img navigate to their respective forms.

---
+++ studioactions/common/AS_FlexContainer_ed43b0a9996743daa82dee911085f76a.js
@@ -0,0 +1,4 @@
+function AS_FlexContainer_ed43b0a9996743daa82dee911085f76a(eventobject) {
+    var self = this;
+    return self.navToRegister.call(this);
+}
---
+++ studioactions/common/AS_FlexContainer_f1ad9ecd7db1470aaecff1939dc175f2.js
@@ -0,0 +1,4 @@
+function AS_FlexContainer_f1ad9ecd7db1470aaecff1939dc175f2(eventobject) {
+    var self = this;
+    return self.navToProfile.call(this);
+}
