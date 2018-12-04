4fe4f66e36d12df39d0ce0fd55389cb1bad9a70e
mirosheytanov <mirosheytanov@gmail.com>
2018-12-04 10:36:08 +0200

    validation commit
    Added basic validation for login form

--- controllers/mobile/LoginFormController/LoginFormController.js
+++ controllers/mobile/LoginFormController/LoginFormController.js
@@ -1,5 +1,35 @@
 define({ 
 
  //Type your controller code here 
+ validate: function() {
+   var email = this.view.txtUsername.text;
+   var password = this.view.txtPassword.text;
+   var validE, validP = false;
+   var ntf = new kony.mvc.Navigation('ProfileForm');
+   
+   if(!kony.string.isValidEmail(email)){
+     validE = false;
+     this.view.flxBorderUsername.skin = 'flxBorderErrSkin';
+     this.view.lblErrUsername.isVisible = true;
+   }else{
+     validE = true;
+     this.view.flxBorderUsername.skin = 'flxDefBorderSkin';
+     this.view.lblErrUsername.isVisible = false;
+   }
+   
+   if((password === null) || (password.length < 1)){
+     validP = false;
+     this.view.flxBorderPassword.skin = 'flxBorderErrSkin';
+     this.view.lblErrPassword.isVisible = true;
+   }else{
+     validP = true;
+     this.view.flxBorderPassword.skin = 'flxDefBorderSkin';
+     this.view.lblErrPassword.isVisible = false;
+   }
+   
+   if(validE && validP){
+     ntf.navigate();
+   }
+ }
 
  });
