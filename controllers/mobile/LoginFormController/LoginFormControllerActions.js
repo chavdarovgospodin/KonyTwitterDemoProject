2f0fcc6395b189e4db144e1530717384e95b310b
Kristiyan Pelteshki <kpelt@softserveinc.com>
2018-12-04 12:20:19 +0200

    successfully fixed Login Height

--- controllers/mobile/LoginFormController/LoginFormControllerActions.js
+++ controllers/mobile/LoginFormController/LoginFormControllerActions.js
@@ -2,113 +2,113 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
-    /** onBeginEditing defined for txtUsername **/
-    AS_TextField_i320e1eed46b47fbb580cf58ce5574f2: function AS_TextField_i320e1eed46b47fbb580cf58ce5574f2(eventobject, changedtext) {
-        var self = this;
-        if (kony.theme.getCurrentTheme() != "default") {
-            kony.theme.setCurrentTheme("default", function() {
-                self.view.flxBorderUsername.skin = "flxBorderFocusSkin";
-            }, null);
-        } else {
-            (function() {
-                self.view.flxBorderUsername.skin = "flxBorderFocusSkin";
-            })();
-        }
-    },
-    /** onEndEditing defined for txtUsername **/
-    AS_TextField_ga21dd53ca9449baaf136671ffd3369c: function AS_TextField_ga21dd53ca9449baaf136671ffd3369c(eventobject, changedtext) {
-        var self = this;
-        if (kony.theme.getCurrentTheme() != "default") {
-            kony.theme.setCurrentTheme("default", function() {
-                self.view.flxBorderUsername.skin = "flxDefBorderSkin";
-            }, null);
-        } else {
-            (function() {
-                self.view.flxBorderUsername.skin = "flxDefBorderSkin";
-            })();
-        }
-    },
-    /** onBeginEditing defined for txtUsername **/
-    AS_TextField_e20ed2c805e04ad5b3100b8876a32721: function AS_TextField_e20ed2c805e04ad5b3100b8876a32721(eventobject, changedtext) {
-        var self = this;
-        if (kony.theme.getCurrentTheme() != "default") {
-            kony.theme.setCurrentTheme("default", function() {
-                self.view.flxBorderUsername.skin = "flxBorderFocusSkin";
-            }, null);
-        } else {
-            (function() {
-                self.view.flxBorderUsername.skin = "flxBorderFocusSkin";
-            })();
-        }
-    },
-    /** onEndEditing defined for txtUsername **/
-    AS_TextField_edd87769cc6b4f13b01d91dbe08854cb: function AS_TextField_edd87769cc6b4f13b01d91dbe08854cb(eventobject, changedtext) {
-        var self = this;
-        if (kony.theme.getCurrentTheme() != "default") {
-            kony.theme.setCurrentTheme("default", function() {
-                self.view.flxBorderUsername.skin = "flxDefBorderSkin";
-            }, null);
-        } else {
-            (function() {
-                self.view.flxBorderUsername.skin = "flxDefBorderSkin";
-            })();
-        }
-    },
-    /** onBeginEditing defined for txtPassword **/
-    AS_TextField_c3c8b3b833a840a7be75bad6d254038f: function AS_TextField_c3c8b3b833a840a7be75bad6d254038f(eventobject, changedtext) {
-        var self = this;
-        if (kony.theme.getCurrentTheme() != "default") {
-            kony.theme.setCurrentTheme("default", function() {
-                self.view.flxBorderPassword.skin = "flxBorderFocusSkin";
-            }, null);
-        } else {
-            (function() {
-                self.view.flxBorderPassword.skin = "flxBorderFocusSkin";
-            })();
-        }
-    },
-    /** onEndEditing defined for txtPassword **/
-    AS_TextField_a5d6863ab3c6499e94cde63246e0e8f3: function AS_TextField_a5d6863ab3c6499e94cde63246e0e8f3(eventobject, changedtext) {
-        var self = this;
-        if (kony.theme.getCurrentTheme() != "default") {
-            kony.theme.setCurrentTheme("default", function() {
-                self.view.flxBorderPassword.skin = "flxDefBorderSkin";
-            }, null);
-        } else {
-            (function() {
-                self.view.flxBorderPassword.skin = "flxDefBorderSkin";
-            })();
-        }
-    },
-    /** onBeginEditing defined for txtPassword **/
-    AS_TextField_h658168e611443b0afb9b6dadc597aef: function AS_TextField_h658168e611443b0afb9b6dadc597aef(eventobject, changedtext) {
-        var self = this;
-        if (kony.theme.getCurrentTheme() != "default") {
-            kony.theme.setCurrentTheme("default", function() {
-                self.view.flxBorderPassword.skin = "flxBorderFocusSkin";
-            }, null);
-        } else {
-            (function() {
-                self.view.flxBorderPassword.skin = "flxBorderFocusSkin";
-            })();
-        }
-    },
-    /** onEndEditing defined for txtPassword **/
-    AS_TextField_h89b80ed5be4414b98dceff5a82e0763: function AS_TextField_h89b80ed5be4414b98dceff5a82e0763(eventobject, changedtext) {
-        var self = this;
-        if (kony.theme.getCurrentTheme() != "default") {
-            kony.theme.setCurrentTheme("default", function() {
-                self.view.flxBorderPassword.skin = "flxDefBorderSkin";
-            }, null);
-        } else {
-            (function() {
-                self.view.flxBorderPassword.skin = "flxDefBorderSkin";
-            })();
-        }
-    },
     /** onClick defined for FooterComp **/
     AS_UWI_e4ffd94e86a14b1586031cb16b81eee2: function AS_UWI_e4ffd94e86a14b1586031cb16b81eee2(eventobject) {
         var self = this;
         return self.validate.call(this);
+    },
+    /** onBeginEditing defined for txtPassword **/
+    AS_TextField_c818be21c89549d1a8ea32d0bddc4c1f: function AS_TextField_c818be21c89549d1a8ea32d0bddc4c1f(eventobject, changedtext) {
+        var self = this;
+        if (kony.theme.getCurrentTheme() != "default") {
+            kony.theme.setCurrentTheme("default", function() {
+                self.view.flxBorderPassword.skin = "flxBorderFocusSkin";
+            }, null);
+        } else {
+            (function() {
+                self.view.flxBorderPassword.skin = "flxBorderFocusSkin";
+            })();
+        }
+    },
+    /** onEndEditing defined for txtPassword **/
+    AS_TextField_hec2ebe65d32436bba9a9fbcf69cde16: function AS_TextField_hec2ebe65d32436bba9a9fbcf69cde16(eventobject, changedtext) {
+        var self = this;
+        if (kony.theme.getCurrentTheme() != "default") {
+            kony.theme.setCurrentTheme("default", function() {
+                self.view.flxBorderPassword.skin = "flxDefBorderSkin";
+            }, null);
+        } else {
+            (function() {
+                self.view.flxBorderPassword.skin = "flxDefBorderSkin";
+            })();
+        }
+    },
+    /** onBeginEditing defined for txtPassword **/
+    AS_TextField_ddafa045fdd24560bcadc14a20356f71: function AS_TextField_ddafa045fdd24560bcadc14a20356f71(eventobject, changedtext) {
+        var self = this;
+        if (kony.theme.getCurrentTheme() != "default") {
+            kony.theme.setCurrentTheme("default", function() {
+                self.view.flxBorderPassword.skin = "flxBorderFocusSkin";
+            }, null);
+        } else {
+            (function() {
+                self.view.flxBorderPassword.skin = "flxBorderFocusSkin";
+            })();
+        }
+    },
+    /** onEndEditing defined for txtPassword **/
+    AS_TextField_a449eda0a5434f088df59baa422fb267: function AS_TextField_a449eda0a5434f088df59baa422fb267(eventobject, changedtext) {
+        var self = this;
+        if (kony.theme.getCurrentTheme() != "default") {
+            kony.theme.setCurrentTheme("default", function() {
+                self.view.flxBorderPassword.skin = "flxDefBorderSkin";
+            }, null);
+        } else {
+            (function() {
+                self.view.flxBorderPassword.skin = "flxDefBorderSkin";
+            })();
+        }
+    },
+    /** onBeginEditing defined for txtUsername **/
+    AS_TextField_bfa40862f162450b9b6f7f2464e06752: function AS_TextField_bfa40862f162450b9b6f7f2464e06752(eventobject, changedtext) {
+        var self = this;
+        if (kony.theme.getCurrentTheme() != "default") {
+            kony.theme.setCurrentTheme("default", function() {
+                self.view.flxBorderUsername.skin = "flxBorderFocusSkin";
+            }, null);
+        } else {
+            (function() {
+                self.view.flxBorderUsername.skin = "flxBorderFocusSkin";
+            })();
+        }
+    },
+    /** onEndEditing defined for txtUsername **/
+    AS_TextField_c63c37ca1f0848b188d9b9764714e6ad: function AS_TextField_c63c37ca1f0848b188d9b9764714e6ad(eventobject, changedtext) {
+        var self = this;
+        if (kony.theme.getCurrentTheme() != "default") {
+            kony.theme.setCurrentTheme("default", function() {
+                self.view.flxBorderUsername.skin = "flxDefBorderSkin";
+            }, null);
+        } else {
+            (function() {
+                self.view.flxBorderUsername.skin = "flxDefBorderSkin";
+            })();
+        }
+    },
+    /** onBeginEditing defined for txtUsername **/
+    AS_TextField_h8037b889198453bbfbec006467590bb: function AS_TextField_h8037b889198453bbfbec006467590bb(eventobject, changedtext) {
+        var self = this;
+        if (kony.theme.getCurrentTheme() != "default") {
+            kony.theme.setCurrentTheme("default", function() {
+                self.view.flxBorderUsername.skin = "flxBorderFocusSkin";
+            }, null);
+        } else {
+            (function() {
+                self.view.flxBorderUsername.skin = "flxBorderFocusSkin";
+            })();
+        }
+    },
+    /** onEndEditing defined for txtUsername **/
+    AS_TextField_d4b806d6c04b49cca780b833d19f952a: function AS_TextField_d4b806d6c04b49cca780b833d19f952a(eventobject, changedtext) {
+        var self = this;
+        if (kony.theme.getCurrentTheme() != "default") {
+            kony.theme.setCurrentTheme("default", function() {
+                self.view.flxBorderUsername.skin = "flxDefBorderSkin";
+            }, null);
+        } else {
+            (function() {
+                self.view.flxBorderUsername.skin = "flxDefBorderSkin";
+            })();
+        }
     }
 });
