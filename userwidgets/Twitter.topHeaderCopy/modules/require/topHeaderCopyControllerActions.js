9cfff9fc48ef7f9cf1b904c5285b19b2b6cb4235
dstoy <dstoy@softserveinc.com>
2018-12-05 15:20:46 +0200

    Adding Form TweetReplies

--- userwidgets/Twitter.topHeaderCopy/modules/require/topHeaderCopyControllerActions.js
+++ userwidgets/Twitter.topHeaderCopy/modules/require/topHeaderCopyControllerActions.js
@@ -3,17 +3,17 @@ define({
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
     /** onClick defined for flxImg **/
-    AS_FlexContainer_c2c4a6b972144c9797136660f0c8b30f: function AS_FlexContainer_c2c4a6b972144c9797136660f0c8b30f(eventobject) {
+    AS_FlexContainer_c5ab266be02d4df695cf1392facc23dc: function AS_FlexContainer_c5ab266be02d4df695cf1392facc23dc(eventobject) {
         var self = this;
         return self.navToProfile.call(this);
     },
     /** onClick defined for flxBackBtn **/
-    AS_FlexContainer_cfcb9117e5864a23aa3148cb4a784eed: function AS_FlexContainer_cfcb9117e5864a23aa3148cb4a784eed(eventobject) {
+    AS_FlexContainer_f7e3f1c30b884c5cbaf931d12a5d6981: function AS_FlexContainer_f7e3f1c30b884c5cbaf931d12a5d6981(eventobject) {
         var self = this;
         return self.navToPrevious.call(this);
     },
     /** onClick defined for flxSignUp **/
-    AS_FlexContainer_de4780cecd544b3697811cb570e0fca9: function AS_FlexContainer_de4780cecd544b3697811cb570e0fca9(eventobject) {
+    AS_FlexContainer_e172d1d15fc549f98a427ae91454290c: function AS_FlexContainer_e172d1d15fc549f98a427ae91454290c(eventobject) {
         var self = this;
         return self.navToRegister.call(this);
     }
