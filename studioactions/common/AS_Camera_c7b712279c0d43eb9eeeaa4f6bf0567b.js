a52bfc06546882b1d7c5e60d40c68c12660ffc05
mirosheytanov <mirosheytanov@gmail.com>
2018-12-05 15:37:51 +0200

    fix image flex boxes

---
+++ studioactions/common/AS_Camera_c7b712279c0d43eb9eeeaa4f6bf0567b.js
@@ -0,0 +1,4 @@
+function AS_Camera_c7b712279c0d43eb9eeeaa4f6bf0567b(eventobject) {
+    var self = this;
+    return self.captureProfileImg.call(this, eventobject);
+}
--- userwidgets/ProfileBio.Profile/modules/require/ProfileControllerActions.js
+++ userwidgets/ProfileBio.Profile/modules/require/ProfileControllerActions.js
@@ -2,14 +2,14 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
-    /** onCapture defined for camProfileImage **/
-    AS_Camera_cfb5d56ecef047e4a27089b3c5b76691: function AS_Camera_cfb5d56ecef047e4a27089b3c5b76691(eventobject) {
-        var self = this;
-        return self.captureProfileImg.call(this, eventobject);
-    },
     /** onCapture defined for camCoverPhoto **/
-    AS_Camera_a5ced21c3e5648118acb3b0914364447: function AS_Camera_a5ced21c3e5648118acb3b0914364447(eventobject) {
+    AS_Camera_e189f09a907943e0bb9a22b955c6889b: function AS_Camera_e189f09a907943e0bb9a22b955c6889b(eventobject) {
         var self = this;
         return self.captureCoverImg.call(this, eventobject);
+    },
+    /** onCapture defined for camProfileImage **/
+    AS_Camera_c7b712279c0d43eb9eeeaa4f6bf0567b: function AS_Camera_c7b712279c0d43eb9eeeaa4f6bf0567b(eventobject) {
+        var self = this;
+        return self.captureProfileImg.call(this, eventobject);
     }
 });
