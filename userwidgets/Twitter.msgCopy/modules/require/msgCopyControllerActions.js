2ab0ca7379bfc53c961ef19df8857c0ba9e92c4d
dstoy <dstoy@softserveinc.com>
2018-12-05 15:17:19 +0200

    AddingFormTweetReplies
    Test

---
+++ userwidgets/Twitter.msgCopy/modules/require/msgCopyControllerActions.js
@@ -0,0 +1,10 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for flxImg **/
+    AS_FlexContainer_b8c4dc73e76f45fda55498cfb783e786: function AS_FlexContainer_b8c4dc73e76f45fda55498cfb783e786(eventobject) {
+        var self = this;
+        return self.navToProfile.call(this);
+    }
+});
---
+++ userwidgets/Twitter.topHeaderCopy/modules/require/topHeaderCopyController.js
@@ -0,0 +1,30 @@
+define(function() {
+
+	return {
+		constructor: function(baseConfig, layoutConfig, pspConfig) {
+
+		},
+		//Logic for getters/setters of custom properties
+		initGettersSetters: function() {
+
+		},
+      
+      
+      navToPrevious: function () {
+        let nav = new kony.mvc.Navigation(kony.application.getPreviousForm().id);
+        nav.navigate();
+      },
+      
+      navToRegister: function () {
+        let nav = new kony.mvc.Navigation('RegisterForm');
+        nav.navigate();
+      },
+      
+      navToProfile: function () {
+        let params = {};
+        
+        let nav = new kony.mvc.Navigation('ProfileForm');
+        nav.navigate(params);
+      }
+	};
+});
