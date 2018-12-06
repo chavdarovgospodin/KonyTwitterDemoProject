f7429f746157c7943a5ed1ecff692179360117ff
Kristiyan Pelteshki <kpelt@softserveinc.com>
2018-12-06 18:35:59 +0200

    updated ListingForm

--- controllers/mobile/LoginFormController/LoginFormController.js
+++ controllers/mobile/LoginFormController/LoginFormController.js
@@ -32,7 +32,7 @@ define({
      //check if email / username exists in database
      //check if password is valid for that user
      var userDto = new UserDto();
-     userDto.signIn( email, password, this.successLogin, this.failLogin);
+     userDto.signIn( email, password, this.successLogin, this.failLogin); //email or username
      
      
      ntf.navigate(); // delete this later so it only navigates on successful user and pass match
