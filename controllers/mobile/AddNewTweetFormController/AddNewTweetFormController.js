bba11268750342351dc00a28d0edf39606e7d619
chavdarovgospodin <g.chavdarov92@gmail.com>
2019-01-10 17:56:12 +0200

    fixing data commit
    fixing date format

--- controllers/mobile/AddNewTweetFormController/AddNewTweetFormController.js
+++ controllers/mobile/AddNewTweetFormController/AddNewTweetFormController.js
@@ -9,17 +9,15 @@ define({
 	var key = kony.store.getItem('userInfo');
     var tweetData;
     var d = new Date();
-    d = new Date(d.getTime() - 3000000);
-    var date_format_str = d.getFullYear().toString()+"-"+((d.getMonth()+1).toString().length==2?(d.getMonth()+1).toString():"0"+(d.getMonth()+1).toString())+"-"+(d.getDate().toString().length==2?d.getDate().toString():"0"+d.getDate().toString())+" "+(d.getHours().toString().length==2?d.getHours().toString():"0"+d.getHours().toString())+":"+((parseInt(d.getMinutes()/5)*5).toString().length==2?(parseInt(d.getMinutes()/5)*5).toString():"0"+(parseInt(d.getMinutes()/5)*5).toString())+":00";
-
     if (this.view.TextAreaNewTweet.text !== '') {
       tweetData = {
         userId: key.id,
         content: this.view.TextAreaNewTweet.text,
-        date: d,
+        date: d.toISOString(),
         locLatitude: 0,
         locLongitude:0
       };
+      alert(JSON.stringify(tweetData));
       tweet.createTweet(tweetData, this.successfulTweet,this.error );
     }
     else {
